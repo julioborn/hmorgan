f070beb9b51555dcf3481b1de72636fb3e7c91fd
julioborn <juliobornes10@gmail.com>
2025-11-20 11:33:33 -0300

    prueba 2

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -36,7 +36,9 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
                             ? "bg-black text-white p-0"
                             : "bg-white text-black px-4 pb-6 container mx-auto"
                             }`}
-                        style={{ paddingTop: "calc(env(safe-area-inset-top) + 108px)" }}
+                        style={{
+                            paddingTop: "calc(env(safe-area-inset-top) + 150px)"
+                        }}
                     >
                         {children}
                     </main>
