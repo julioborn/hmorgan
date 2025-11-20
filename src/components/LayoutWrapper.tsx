1805043c6b9ef86e60d63f26d2017d3834b6d98d
julioborn <juliobornes10@gmail.com>
2025-11-20 14:17:00 -0300

    header ok 5

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -37,7 +37,8 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
                             : "bg-white text-black px-4 pb-6 container mx-auto"
                             }`}
                         style={{
-                            paddingTop: "calc(env(safe-area-inset-top) + 110px)"
+                            paddingTop: "calc(env(safe-area-inset-top) + 100px)",
+                            marginTop: 0,        // 👈 Asegura que nunca desplace hacia abajo
                         }}
                     >
                         {children}
