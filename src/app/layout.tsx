436c85564f4436cf00dffa10439bcf0a883e42fc
LOS BORNES <losbornes@iMac-de-Grego.local>
2025-12-22 18:19:36 -0300

    layout ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,10 +23,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" style={{ colorScheme: "light" }} className="light">
       <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
-        />
         <meta name="color-scheme" content="light" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
